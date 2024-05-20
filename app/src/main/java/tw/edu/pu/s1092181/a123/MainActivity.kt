38fd32f7b469dbd0cde187aaf3c7d764fea7862e
s1092181 <kmes49@gmail.com>
2024-05-20 16:00:07 +0800

    v3

--- app/src/main/java/tw/edu/pu/s1092181/a123/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1092181/a123/MainActivity.kt
@@ -3,7 +3,9 @@ package tw.edu.pu.s1092181.a123
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,6 +15,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -59,6 +64,26 @@ fun Myself( modifier: Modifier = Modifier) {
                 contentDescription=null,
                 modifier= Modifier.padding(top = 16.dp)
             )
+            Box(
+                modifier = modifier
+                    .fillMaxSize()
+                    .padding(top = 16.dp)
+            ){
+                Canvas(modifier = Modifier.fillMaxSize()) {
+                    val canvasWidth = size.width
+                    val canvasHeight = size.height
+                    drawRect(
+                        color= Color.Blue,
+                        topLeft = Offset(50f, 50f),
+                        size= Size(width = 200f, height = 100f)
+                    )
+                    drawRect(
+                        color= Color.Blue,
+                        topLeft = Offset(-250f,-150f),
+                        size= Size(width = 200f, height = 100f)
+                    )
+                }
+            }
         }
     }
 
